f2c284ee8722949c6788a801304ef3d9344f7825
fancysmartypants <128561269+fancysmartypants@users.noreply.github.com>
2024-05-05 14:40:55 -0700

    Adjust UI to center buttons and outputs. Add event listener and functions  rock button.

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ let pc_score=0;
 function getComputerChoice(){
     const choices=["rock","paper","scissor"];
 
-    const int= Math.round(Math.random()*3);
-
+    const int= Math.floor(Math.random()*3+0.5);
+    console.log(`const is ${int}`)
     return answer=choices[int];
 }
 
@@ -29,18 +29,21 @@ function getHumanChoice(){
 
 
 function playRound(pc,player){
+    
     if (pc===player){
         console.log("Uh-o, it's a match. Try again.");
-        playGame();
+        dashboard.appendChild(liMatch);
+        liMatch.textContent = matchTxt;
+
     }else if ((pc == "rock" || player=="rock") && (pc =="scissor") || (player=="scissor")){
         round_num+=1;
         console.log("incremented round_num="+round_num);
         if (pc=="rock"){
             pc_score+=1;
-            return winner="pc";
+            return winner="PC";
         }else{
             player_score+=1;
-            return winner="player";
+            return winner="Player";
         }
    
     }else if ((pc == "scissor" || player=="scissor") && (pc =="paper") || (player=="paper")) {
@@ -48,51 +51,23 @@ function playRound(pc,player){
         console.log("incremented round_num="+round_num);
         if (pc=="scissor"){
             pc_score+=1;
-            return winner="pc";
+            return winner="PC";
         }else{
             player_score+=1;
-            return winner="player";
+            return winner="Player";
         }
     }else if ((pc == "rock" || player=="rock") && (pc =="paper") || (player=="paper")){
         round_num+=1;
         console.log("incremented round_num="+round_num);
         if (pc=="paper"){
             pc_score+=1;
-            return winner="pc";
+            return winner="PC";
         }else{
             player_score+=1;
-            return winner="player";
+            return winner="Player";
         }
     }       
 }
-
-
-
-function playGame(){
-
-    console.log(`----Starting round ${round_num}----`);
-
-// use while look to playRound five times. 
-// In the end, show score and declare the winner
-
-    while (round_num!=5) {
-        const pcChoice= getComputerChoice();
-        const playerChoice= getHumanChoice();  
-        if (playerChoice == null) break;
-        playRound(pcChoice,playerChoice);
-
-        console.log(`------ROUND ${round_num} RESULT:-------`);
-        console.log("player score is:"+player_score);
-        console.log("pc score is:"+pc_score);
-        console.log(`This round's winner is ${winner}`)
-        console.log(`------END OF ROUND ${round_num}-------`);
-    }
-    console.log(`------GAME OVER-------`);
-    declareResult();
-
-}
-
-
 
 
 let round_num=0;
@@ -111,5 +86,158 @@ function declareResult(){
     console.log(`PC score is: ${pc_score}`);
     finalWinner(player_score,pc_score);
     console.log(`Final winner is: ${final_winner} `)
+}
+
+
+const rock=document.getElementById('r');
+const paper=document.getElementById('p');
+const scissor=document.getElementById('s');
+
+let pcChoice='';
+
+const dashboard = document.getElementById('dashboard');
+
+let playerChoice="";
+
+
+// All of the console output
+
+let playerChoiceTxt;
+
+let pcChoiceTxt;
+
+let roundResult;
+
+let PCScore;
+
+let playerScore;
+
+const matchTxt = "Uh-o, it's a match. Try again.";
+
+//  create references to li element
+const liPlayerChoice = document.createElement("li");
+
+const liPCChoice = document.createElement("li");
+
+const liPlayerScore = document.createElement("li");
+
+const liPCScore = document.createElement("li");
+
+const liRoundResult = document.createElement("li");
+
+const liFinalResult = document.createElement("li");
+
+const liMatch = document.createElement("li");
+
+
+
+
+
+// add event listener to rock button
+rock.addEventListener('click', (e)=>{
+    alert('clicked');
+    playerChoice = "rock";
+    playerChoiceTxt = `You picked ${playerChoice}.`;
+
+    showPlayerChoice();
+
+    playGame(playerChoice);
+    
+    console.log(`winner is ${winner}`);
+    console.log(`------ROUND ${round_num} RESULT:-------`);
+    console.log("player score is:"+player_score);
+    console.log("pc score is:"+pc_score);
+    console.log(`This round's winner is ${winner}`)
+    console.log(`------END OF ROUND ${round_num}-------`);
+
+});
+
+// add event listener to paper button
+paper.addEventListener('click', (e)=>{
+
+    playGame("paper");
+    console.log(`winner is ${winner}`);
+
+});
+
+
+// add event listener to scissor button
+scissor.addEventListener('click', (e)=>{
+
+    playGame("scissor");
+    console.log(`winner is ${winner}`);
+});
+
+
+function displayResult(){
+
+    roundResult = `Round ${round_num} Result: 
+    Winner is ${winner}.`;
+
+    //SHOW RESULTS
+    showResult();
+
+    PCScore = `PC score is ${pc_score}.`;
+    playerScore = `Player score is ${player_score}.`;
+
+    //SHOW SCORES
+    showPlayerScore();
+    showPCScore();
+}
+
+
+
+
+function showPlayerChoice(){
+    dashboard.appendChild(liPlayerChoice);
+    liPlayerChoice.textContent = playerChoiceTxt;
+}
+
+
+
+
+function showPlayerScore(){
+    dashboard.appendChild(liPlayerScore);
+    liPlayerScore.textContent = playerScore;
+}
+
+
+
+
+function showPCScore(){
+    dashboard.appendChild(liPCScore);
+    liPCScore.textContent=PCScore;
+}
+
+function showPCChoice(){
+    dashboard.appendChild(liPCChoice);
+    liPCChoice.textContent=pcChoiceTxt;
+
+}
+
+
+
+
+
+function showResult(){
+    dashboard.appendChild(liRoundResult);
+    liRoundResult.textContent=roundResult;
+}
+
+
+
+function playGame(playerChoice){
+    pcChoice = getComputerChoice();
+    console.log(`pc choice is ${pcChoice} within the playerChoice function`);
+    //SHOW CHOICES
+    pcChoiceTxt = `PC picked ${pcChoice}.` ;
+    
+    showPCChoice();
+
+    playRound(pcChoice,playerChoice);
+
+
+
+    displayResult();
 }
 
